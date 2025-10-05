d225c67a350b51bc533ff5ecf663d5675748e301
rakeshk110 <rakeshrh6767.email.com>
2025-10-05 18:57:44 +0530

    tem using sqlite

--- Habit_tracker/settings.py
+++ Habit_tracker/settings.py
@@ -77,13 +77,17 @@ WSGI_APPLICATION = 'Habit_tracker.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'habit_db',
-        'USER': 'habituser',
-        'PASSWORD': 'rakesh',
-        'HOST': 'localhost',
-        'PORT': '3306',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'habit_db',
+    #     'USER': 'habituser',
+    #     'PASSWORD': 'rakesh',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
+      'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
